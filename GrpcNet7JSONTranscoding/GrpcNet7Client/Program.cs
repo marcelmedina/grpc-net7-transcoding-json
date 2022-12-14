0b538a71c2a1016a7a8cdae8c6900eed507e4b2b
Marcel Medina <marcel.net@gmail.com>
2022-12-14 20:59:26 +1300

    grpc-net7-transcoding-json

---
+++ GrpcNet7JSONTranscoding/GrpcNet7Client/Program.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Net.Client;
+using System.Text.Json;
+using GrpcNet7JSONTranscoding.Protos;
+using System.Net.Http;
+
+var userId = 1;
+
+// gRPC
+var channel = GrpcChannel.ForAddress("https://localhost:7060");
+var client = new UserService.UserServiceClient(channel);
+var userDetails = await client.GetUserDetailsAsync(new UserRequest() { UserId = userId });
+Console.WriteLine(JsonSerializer.Serialize(userDetails));
+
+// REST API
+var httpClient = new HttpClient();
+var request = new HttpRequestMessage(HttpMethod.Get, $"https://localhost:7060/user/{userId}") { Version = new Version(2, 0) };
+var response = await httpClient.SendAsync(request);
+response.EnsureSuccessStatusCode();
+var content = await response.Content.ReadAsStringAsync();
+Console.WriteLine(content);
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
---
+++ GrpcNet7JSONTranscoding/GrpcNet7JSONTranscoding/Services/UserService.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Core;
+using GrpcNet7JSONTranscoding.Protos;
+
+namespace GrpcNet7JSONTranscoding.Services
+{
+    public class UserService : Protos.UserService.UserServiceBase
+    {
+        private readonly ILogger<UserService> _logger;
+        public UserService(ILogger<UserService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<UserResponse> GetUserDetails(UserRequest request, ServerCallContext context)
+        {
+            var userResponse = new UserResponse
+            {
+                UserId = 1,
+                FirstName = "Bill",
+                LastName = "Lumbergh",
+                Address = new Address()
+                {
+                    Number = "1A",
+                    StreetName = "Initech Street",
+                    Suburb = "Initech Suburb",
+                    City = "Austin, Texas",
+                    Country = "USA"
+                }
+            };
+
+            return Task.FromResult(userResponse);
+        }
+    }
+}
